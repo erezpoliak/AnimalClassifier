286ebaa815472cd66b6791e1fa5cf1cd92fb1339
erezpoliak <poliakerez@gmail.com>
2026-01-11 17:58:44 +0200

    .

--- EX4 - Project/CustomCNN.py
+++ EX4 - Project/CustomCNN.py
@@ -22,7 +22,7 @@ class CustomCNN(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
 
         #######
-        self.global_pool = nn.AdaptiveAvgPool2d((7, 7))
+        self.global_pool = nn.AdaptiveAvgPool2d((4, 4))
         #######
 
         # Build convolutional layers
@@ -43,7 +43,7 @@ class CustomCNN(nn.Module):
         #     w = w // 2
         # flattened_size = conv_layers[-1] * h * w
         #############
-        flattened_size = conv_layers[-1] * 7 * 7
+        flattened_size = conv_layers[-1] * 4 * 4
         #############
 
         # Build fully connected layers
